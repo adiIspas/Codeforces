0be41f009e73264d303943d7aeaf7986d51aa815
Adrian Ispas <adi.ispas95@gmail.com>
2016-09-20 16:40:02 +0300

    Finish A - Crazy Computer

---
+++ Round372/main.py
@@ -0,0 +1,17 @@
+number, c = list(map(int, input().split()))
+
+seconds = list(map(int, input().split()))
+
+words = 0
+for i in range(0, number - 1):
+    a = seconds[i]
+    b = seconds[i + 1]
+
+    if b - a <= c:
+        words += 1
+    if b - a > c and words > 0:
+        words = 0
+
+words += 1
+print(words)
+
